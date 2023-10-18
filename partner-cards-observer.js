4774db88421f2ea44b8440469ef058a0b2a426c7
HappyDridex <120603220+HappyDridex@users.noreply.github.com>
2023-10-18 19:34:56 +0300

    first commit

---
+++ partner-cards-observer.js
@@ -0,0 +1,29 @@
+const ENABLE_CARDS_ANIMATION = true;
+
+window.onload = () => {
+    if (ENABLE_CARDS_ANIMATION) {
+        observePartnerCards();
+    }
+};
+
+function observePartnerCards() {
+    const cards = document.querySelectorAll(".partner-card");
+
+    cards.forEach((card) => {
+        card.classList.add("partner-card_observer-animation-hidden");
+    });
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                entry.target.classList.toggle("partner-card_observer-animation-shown", entry.isIntersecting);
+            });
+        },
+        { threshold: 0, rootMargin: "0px -100px 0px 0px" }
+    );
+
+    cards.forEach((card) => {
+        console.log(card);
+        observer.observe(card);
+    });
+}
